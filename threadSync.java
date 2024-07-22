19af19e2f5715f17f22dcada7540befd72a301e9
Anant Shukla <anantshukla404@gmail.com>
2024-07-22 17:19:40 +0530

    learning synchronisation

---
+++ threadSync.java
@@ -0,0 +1,32 @@
+public class threadSync {
+
+    static int count = 0;
+    public static void main(String[] args) throws InterruptedException{
+        
+        Thread one = new Thread(() -> {
+            for(int i=0; i<100000; i++){
+                System.out.println("one is running");
+                count++;
+                System.out.println( "count is :" + count );
+            }
+        });
+
+        Thread two = new Thread(() -> {
+            for(int i=0; i<100000; i++){
+
+                System.out.println("two is running");
+                count++;
+                System.out.println( "count is :" + count );
+            }
+        });
+
+        one.start();
+        two.start();
+
+        one.join();
+        two.join();
+
+        System.out.println("final count is : " + count);
+    }
+}
+
