e5705bf721fb8fa6f0997df10b417af39c5f52d6
Anant Shukla <anantshukla404@gmail.com>
2024-07-23 16:39:36 +0530

    daemon threads and user threads

---
+++ daemonandUser.java
@@ -0,0 +1,46 @@
+import javax.swing.DebugGraphics;
+
+public class daemonandUser {
+    public static void main(String[] args) {
+        
+        Thread bg = new Thread(new DaemonThread());
+        Thread user = new Thread(new UserThread());
+        bg.setDaemon(true);
+        bg.start();
+        user.start();
+
+    }
+}
+
+class DaemonThread implements Runnable {
+
+    @Override
+    public void run(){
+
+        int c = 0;
+        while(c < 50){
+            try{
+                Thread.sleep(1000);
+            }
+            catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+            c++;
+            System.out.println("daemon thread is running");
+        }
+    }
+}
+
+class UserThread implements Runnable{
+
+    @Override
+    public void run(){
+        try{
+            Thread.sleep(5000);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("user thread is running");
+    }
+}
