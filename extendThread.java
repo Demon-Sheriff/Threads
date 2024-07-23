88fa8f9122aafd5e3c49914de7b7bb682df96f9c
Anant Shukla <anantshukla404@gmail.com>
2024-07-23 16:17:44 +0530

    join for threads

---
+++ extendThread.java
@@ -0,0 +1,40 @@
+public class extendThread {
+    public static void main(String[] args) throws InterruptedException{
+        
+        Thread one = new Thread1();
+        Thread two = new Thread2();
+
+        one.start();
+        two.start();
+        
+        one.join(); // make sure that one is done executing.
+        two.join(); // make sure that two is done executing.
+
+        System.out.println("all threads done executing ? or not !? ");
+
+    }
+}
+
+class Thread1 extends Thread {
+
+    @Override
+    public void run(){
+        for(int i=0; i<10; i++){
+            System.out.println(
+                "thread1 : " + i
+            );
+        }
+    }
+}
+
+class Thread2 extends Thread{
+
+    @Override
+    public void run(){
+        for(int i=0; i<20; i++){
+            System.out.println(
+                "thread2 : " + i
+            );
+        }
+    }
+}
